5f0a5f06cec1c974b4bbfcdac86562eb9503c84e
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-10-12 20:36:36 +0700

    feat: generate cells based on parameter

--- Scenes/grid_container.gd
+++ Scenes/grid_container.gd
@@ -1,27 +1,23 @@
 extends GridContainer
 
+var CellScene : PackedScene = preload("res://Scenes/Cell.tscn")
+var mine_count = 10  
+var grid_size_x = 5  
+var grid_size_y = 10  
 var cells = []
-var mine_count = 10
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	cells = get_children()
-	print(cells)
+	for y in range(grid_size_y):
+		for x in range(grid_size_x):
+			var cell_instance = CellScene.instantiate()  
+			add_child(cell_instance)  
+			cells.append(cell_instance)  
 	
-	cells.shuffle()
+	assign_mines()
 
-	# Assign mines to the first mine_count cells
+
+func assign_mines() -> void:
+	cells.shuffle()  
 	for i in range(mine_count):
 		var cell = cells[i]
-		cell.has_mine = true  # Set the mine property on the cell
-		print("Mine placed in cell: ", i)
-
-	# Optional: Ensure other cells do not have mines
-	for i in range(mine_count, cells.size()):
-		var cell = cells[i]
-		cell.has_mine = false
-	
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+		cell.has_mine = true  
